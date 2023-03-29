1c0d634842c9884eadfc95a14daa368f8537595f
ariokusuma <ariopuboyo@gmail.com>
2023-03-29 10:20:42 +0700

    commit baikin login ref #5

--- resources/views/loginRegister/login.blade.php
+++ resources/views/loginRegister/login.blade.php
@@ -15,7 +15,7 @@
     <div class=" pt-8 flex mx-auto place-content-center">
         <div class="max-w-xl p-6 bg-white border border-gray-200 rounded-xl shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
-                <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Halo, Selamat Datang</h5>
+                <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Halo, Selamat Datang Kembali</h5>
             </a>
             <p class="text-center mb-3 font-normal text-gray-700 dark:text-gray-400">Silahkan masukan informasi Anda</p>
 
