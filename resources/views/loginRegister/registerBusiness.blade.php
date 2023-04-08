c36142b96148fb1fbb9e516e36b7e2761b3b0c88
ariokusuma <ariopuboyo@gmail.com>
2023-04-08 23:22:28 +0700

    fix front-end #4

--- resources/views/loginRegister/registerBusiness.blade.php
+++ resources/views/loginRegister/registerBusiness.blade.php
@@ -27,52 +27,52 @@
                     <div class="gap-8 columns-2">
 
                         <div class="mb-2">
-                            <label for="namaDepan" class="block mb-2 t text-gray-900 dark:text-white">Nama Depan</label>
-                            <input type="text" id="namaDepan" name="namaDepan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1" placeholder="Asep" required>
+                            <label for="namaDepan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-Cabin-Medium">Nama Depan</label>
+                            <input type="text" id="namaDepan" name="namaDepan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1 font-Cabin-Medium" placeholder="Asep" required>
                         </div>
 
                         <div class="mb-2">
-                            <label for="namaBelakang" class="block mb-2 t text-gray-900 dark:text-white">Nama Belakang</label>
-                            <input type="text" id="namaBelakang" name="namaBelakang" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1" placeholder="Alfamidi" required>
+                            <label for="namaBelakang" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-Cabin-Medium">Nama Belakang</label>
+                            <input type="text" id="namaBelakang" name="namaBelakang" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1 font-Cabin-Medium" placeholder="Alfamidi" required>
                         </div>
                     </div>
 
                     <div class="mb-2">
-                        <label for="lokasi" class="block mb-2 t text-gray-900 dark:text-white">Lokasi Anda</label>
-                        <input type="text" id="lokasi" name="lokasi"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1" placeholder="Indonesia" required>
+                        <label for="lokasi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-Cabin-Medium">Lokasi Anda</label>
+                        <input type="text" id="lokasi" name="lokasi"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1 font-Cabin-Medium" placeholder="Indonesia" required>
                     </div>
 
                     <div class="mb-2">
-                        <label for="nohp" class="block mb-2 t text-gray-900 dark:text-white">Nomor Hp</label>
-                        <input type="text" id="nohp" name="nohp" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1" placeholder="+62-xxx" required>
+                        <label for="nohp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-Cabin-Medium">Nomor Hp</label>
+                        <input type="text" id="nohp" name="nohp" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1 font-Cabin-Medium" placeholder="+62-xxx" required>
                     </div>
 
                     <div class="mb-2">
-                        <label for="email" class="block mb-2 t text-gray-900 dark:text-white">Masukan Email Anda</label>
-                        <input type="email" id="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1" placeholder="name@mail.com" required>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-Cabin-Medium">Masukan Email Anda</label>
+                        <input type="email" id="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1 font-Cabin-Medium" placeholder="name@mail.com" required>
                     </div>
 
                     <input name="role" id="role" for ="role" type="hidden" value="0">
 
                     <div class="mb-2">
-                        <label for="password" class="block mb-2 t text-gray-900 dark:text-white">Kata Sandi Anda</label>
-                        <input type="password" id="password" name="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1" required>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-Cabin-Medium">Kata Sandi Anda</label>
+                        <input type="password" id="password" name="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1 font-Cabin-Medium" required>
                     </div>
 
                     <div class="mb-2">
-                        <label for="repassword" class="block mb-2 t text-gray-900 dark:text-white">Kata Sandi Anda</label>
-                        <input type="password" id="repassword" name="repassword" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1" required>
+                        <label for="repassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-Cabin-Medium">Kata Sandi Anda</label>
+                        <input type="password" id="repassword" name="repassword" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-hijau1 focus:border-hijau1 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-hijau1 dark:focus:border-hijau1 font-Cabin-Medium" required>
                     </div>
 
                     <div class="pt-4 flex flex-col items-center">
-                        <button type="submit" class="text-white bg-hijau1 hover:bg-hijau2 focus:ring-4 focus:outline-non focus:ring- rounded-lg text-sm sm:w-auto px-40 py-2.5 text-center dark:bg-hijau1 dar:hover:bg-hijau1 dar:focus:ring-hijau1 mx-auto" value="buat akun" >Buat Akun</button>
+                        <button type="submit" class="text-white bg-hijau1 hover:bg-hijau2 focus:ring-4 focus:outline-non focus:ring-hijau3 font-medium rounded-lg text-ms sm:w-auto px-40 py-2.5 text-center dark:bg-hijau1 dar:hover:bg-hijau1 dar:focus:ring-hijau1 mx-auto font-Cabin-Medium" value="buat akun" >Buat Akun</button>
                     </div>
                 </form>
             </div>
 
-            <p class="pt-8 text-center">Sudah Punya Akun? <strong><a class="hover:text-hijau1" href="{{ url('login') }}" class="text-reset text-heavy">Masuk Sekarang</a></strong></p>
+            <p class="pt-8 text-center font-Cabin-Medium">Sudah Punya Akun? <strong><a class="hover:text-hijau1" href="{{ url('login') }}" class="text-reset text-heavy">Masuk Sekarang</a></strong></p>
 
-            {{-- <a href="#" class="inline-flex items-center px-3 py-2 t text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            {{-- <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Read more
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-2 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a> --}}
