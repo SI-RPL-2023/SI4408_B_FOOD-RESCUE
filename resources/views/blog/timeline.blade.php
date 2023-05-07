ebb48d2799aa86172221a6b0ab5982bc2a8f5653
ariadanidf <ariadanidf@gmail.com>
2023-05-08 00:46:48 +0700

    Add Text Overflow ref #42

--- resources/views/blog/timeline.blade.php
+++ resources/views/blog/timeline.blade.php
@@ -24,7 +24,7 @@
                         <img class="h-44 w-full object-cover mx-auto rounded-lg" src="/upload/blog/{{ $blog->foto }}" alt="Blog image">
                     </div>
                     <h2 class="mb-2 text-2xl font-cabin-medium font-bold tracking-tight text-gray-900 dark:text-white"><a href="{{ url('detail-blog/' .$blog->id) }}"> {{ $blog->judul }}</a></h2>
-                    <p class="mb-5 font-cabin text-gray-500 dark:text-gray-400">{{ $blog->deskripsi }}</p>
+                    <p class="mb-5 font-cabin text-gray-500 dark:text-gray-400 line-clamp-3">{{ $blog->deskripsi }}</p>
                     <div class="flex justify-between items-center">
                         <div class="flex items-center space-x-4">
                             <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Admin avatar" />
