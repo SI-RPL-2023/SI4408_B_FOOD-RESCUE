52d555f26dcd221979fb8d3b2f0f650be3af5b0d
ariadanidf <ariadanidf@gmail.com>
2023-05-17 09:25:14 +0700

    add fix

--- resources/views/blog/timeline.blade.php
+++ resources/views/blog/timeline.blade.php
@@ -28,7 +28,7 @@
                     <div class="flex justify-between items-center">
                         <div class="flex items-center space-x-4">
                             <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Admin avatar" />
-                            <span class="font-Cabin-medium dark:text-white">
+                            <span class="font-Cabin-medium dark:text-primary-500">
                                 Admin Food Rescue
                             </span>
                         </div>
